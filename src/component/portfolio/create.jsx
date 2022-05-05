06786e87ee5821e2386e90bb99c65ef491a7ff61
AbeTetsuya20 <abetetsuya@abetetsuyanoMacBook-Air.local>
2022-05-05 10:00:21 +0900

    creatタブの編集

--- src/component/portfolio/create.jsx
+++ src/component/portfolio/create.jsx
@@ -1,51 +1,94 @@
 import "../../stylesheet/styles.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Autoplay, Swiper as RealSwiper } from "swiper";
+import Footer from "../parts/footer";
 
-import "swiper/css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 const Create = () => {
+  RealSwiper.use([Autoplay]);
   return (
     <div className="create_body">
+      <div>
+        <h1>作成したプログラム一覧</h1>
+      </div>
+
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
+        spaceBetween={10}
         slidesPerView={1}
-        navigation
+        navigation={true}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
+        autoHeight={true}
+        centerInsufficientSlides={true}
+        // loop={true}
       >
         <SwiperSlide>
           <div className="swipelist">
             <h1> 1. このポートフォリオサイト </h1>
-            <h4> 使用言語: HTML, CSS, React, Javascript </h4>
-            <h4> ああああああああ </h4>
-            <h2> 1/3 </h2>
+            <ul>
+              <li>
+                <div> 使用言語</div>
+              </li>
+              <p>Go 言語</p>
+              <li>
+                <div> 機能概要 </div>
+              </li>
+              <p> mattermost で出席管理ができる bot を作成しました。 </p>
+              <li>
+                <div>使い方</div>
+              </li>
+              <p> mattermost のチャット欄で「/attend」 </p>
+            </ul>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swipelist">
             <h1> 2. Mattermost BOT </h1>
-            <h4> 使用言語: Go 言語 </h4>
-            <h4> ああああああああ </h4>
-            <h2> 2/3 </h2>
+            <ul>
+              <li>
+                <div> 使用言語</div>
+              </li>
+              <p>Go 言語</p>
+              <li>
+                <div> 機能概要 </div>
+              </li>
+              <p> mattermost で出席管理ができる bot を作成しました。 </p>
+              <li>
+                <div>使い方</div>
+              </li>
+              <p> mattermost のチャット欄で「/attend」 </p>
+            </ul>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swipelist">
             <h1> 3. Python で自然言語処理 </h1>
-            <h4> 使用言語: Python </h4>
-            <h4> ああああああああ </h4>
-            <h2> 3/3 </h2>
+            <ul>
+              <li>
+                <div> 使用言語</div>
+              </li>
+              <p>Go 言語</p>
+              <li>
+                <div> 機能概要 </div>
+              </li>
+              <p> mattermost で出席管理ができる bot を作成しました。 </p>
+              <li>
+                <div>使い方</div>
+              </li>
+              <p> mattermost のチャット欄で「/attend」 </p>
+            </ul>
           </div>
         </SwiperSlide>
       </Swiper>
+      <Footer />
     </div>
   );
 };
--- src/component/portfolio/history.jsx
+++ src/component/portfolio/history.jsx
@@ -15,48 +15,51 @@ const History = () => {
 
   return (
     <div className="site_body">
+      <div className="item purple"></div>
+      <div className="item medium-blue"></div>
+      <div className="item light-blue"></div>
+      <div className="item red"></div>
+      <div className="item orange"></div>
       <Box>
         <Stack spacing={2}>
           <Item>
             <div className="element1">
               <h4>桐蔭学園高等学校普通科</h4>
-              剣道部に所属していました。 数学が好きでした。
+              剣道部に所属していた。 数学が好きで英語が嫌いだった。
             </div>
           </Item>
           <h1 className="while">↓</h1>
           <Item>
             <div className="element1">
               <h4>法政大学 1 年生</h4>
-              RAT　に所属しました。
+              <a
+                href="https://rat.cis.k.hosei.ac.jp"
+                target="_blank"
+                rel="noreferrer"
+              >
+                RAT
+              </a>
+              に所属しました。
             </div>
           </Item>
           <h1 className="while">↓</h1>
           <Item>
             <div className="element1">
-              <h4>桐蔭学園</h4>
-              数学が好きでした
+              <h4>法政大学 2 年生</h4>
+              <a href="https://metadata.co.jp/">metadata 株式会社</a>に所属
+              <br></br>
+              react や ruby on rails、自然言語処理について学んだ。<br></br>
+              主に、react で単語の関係のグラフを作成をし、rails
+              でコントロールをさせた。
             </div>
           </Item>
           <h1 className="while">↓</h1>
           <Item>
             <div className="element1">
-              <h4>桐蔭学園</h4>
-              数学が好きでした
+              <h4>法政大学 3 年生</h4>
+              <a href="https://appify.jp/">Appify</a>社に所属 <br></br>
             </div>
           </Item>
-          <div className="item purple"></div>
-          <div className="item medium-blue"></div>
-          <div className="item light-blue"></div>
-          <div className="item red"></div>
-          <div className="item orange"></div>
-          <div className="item orange"></div>
-          <div className="item yellow"></div>
-          <div className="item cyan"></div>
-          <div className="item light-green"></div>
-          <div className="item lime"></div>
-          <div className="item magenta"></div>
-          <div className="item lightish-red"></div>
-          <div className="item pink"></div>
         </Stack>
       </Box>
     </div>
